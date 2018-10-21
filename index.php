b2f1c52f2cc92c764a9eabdf389dc61f5b911831
fmitin <fmitin@ivi.ru>
2018-10-21 14:01:11 +0300

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Memento</title>
+</head>
+<body>
+	<h1>Это тестовая выгрузка приложения на beget.com</h1>
+</body>
+</html>
